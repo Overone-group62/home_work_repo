37b1faa59846d6392b1eecee7f029b42af5dda04
MelnichenkoOksana <83362953+MelnichenkoOksana@users.noreply.github.com>
2021-05-27 22:12:25 +0300

    Морской бой

---
+++ DotComBust.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class DotComBust {
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComsList =new ArrayList<DotCom>();
+    private int nomOfGuesses =0;
+
+    private void setUpGame() {
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+
+        System.out.println("Ваша цель потопить три 'сайта'. ");
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Попытайтесь потопить их за минимальное количество ходов");
+
+        for (DotCom dotComToSet: dotComsList){
+            ArrayList<String> newLocation =helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+            }
+    }
+
+    private void startPlaying() {
+        while (!dotComsList.isEmpty()){
+            String userGuess = helper.getUserInput("Сделайте ход");
+            checkUserGuess (userGuess);
+        }
+        finishGame();
+    }
+
+    private void checkUserGuess (String userGuess){
+        nomOfGuesses++;
+        String result = "Мимо";
+        for (DotCom dotComToTest: dotComsList) {
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("Попал")){
+                break;
+            }
+            if (result.equals("Потопил")){
+                dotComsList.remove(dotComToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame(){
+        System.out.println("Все сайты пошли ко дну");
+        if (nomOfGuesses <= 18) {
+            System.out.println( "Вы потратили всего " +nomOfGuesses+ " попыток. Хороший результат!");
+        } else {
+            System.out.println("Вы потратили " +nomOfGuesses+ " попыток. ");
+        }
+    }
+
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }
+
+}
